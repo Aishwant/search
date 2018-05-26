03a643f9f0569f58e6cccdfe9a2cdbdea48ef7cc
Aishwant <aishwantg@gmail.com>
2018-05-26 11:45:58 -0400

    Search with user defined question and options with functionality

--- matchText/main.py
+++ matchText/main.py
@@ -2,16 +2,32 @@
 
 from google import google
 
+query = raw_input("The question: ");
+
+option = []
+
+for i in range(0,3):
+    s = "Option "+str(i+1)+": ";
+    option.append(raw_input(s).lower())
 
 num_page = 1
 
-search_result = google.search("which of the tv shows is still running", num_page)
+search_result = google.search(query, num_page)
+r={}
 
-print search_result[1]
-print search_result[1].description
+for i in range(0,len(search_result)):
+    print search_result[i]
 
-result = search_result[1].description.lower()
+    result = search_result[i].description.lower()
+    if option[0] in result:
+        try: r[option[0]]=r[option[0]]+1
+        except: r[option[0]]=1
+    if option[1] in result:
+        try: r[option[1]]=r[option[1]]+1
+        except: r[option[1]]=1
+    if option[2] in result:
+        try: r[option[2]]=r[option[2]]+1
+        except: r[option[2]]=1
 
-a = "black mirror" in result
-
-print a
+print len(search_result)
+print r
